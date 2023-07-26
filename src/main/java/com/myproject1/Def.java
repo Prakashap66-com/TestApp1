43f4c238eb3af14cc29dba9907b1a1ebe6884170
anshu1706 <prakashanshu491@gmail.com>
2023-07-26 12:51:57 +0530

    first commit

---
+++ src/main/java/com/myproject1/Def.java
@@ -0,0 +1,13 @@
+package com.myproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Def 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/myproject1/Ghi.java
@@ -0,0 +1,13 @@
+package com.myproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ghi
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
