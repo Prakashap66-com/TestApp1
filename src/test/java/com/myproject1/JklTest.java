463a2681248f8f7e5153ef0a68bc23efe8975c53
anshu1706 <prakashanshu491@gmail.com>
2023-07-26 12:57:55 +0530

    second commit

---
+++ src/test/java/com/myproject1/JklTest.java
@@ -0,0 +1,20 @@
+package com.myproject1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class JklTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
